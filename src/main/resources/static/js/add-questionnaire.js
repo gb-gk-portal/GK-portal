fdfc9501c7b00da64f4f205344e9007a477fc697
YURY TILMAN <uatilman@gmail.com>
2019-03-03 15:19:03 +0300

    Add comments to add-questionnaire.js and small refactoring

--- src/main/resources/static/js/add-questionnaire.js
+++ src/main/resources/static/js/add-questionnaire.js
@@ -1,18 +1,23 @@
-var lastQuestionIndex=1;
-var templateVars = {questionIndex :lastQuestionIndex, answerIndex : 2};
+var lastQuestionIndex = 1;
+var templateVars = {questionIndex: lastQuestionIndex, answerIndex: 2};
 
 $(document).ready(function () {
     lastQuestionIndex = $('.question-template').length;
+
+    // из html шаблона по id #questionTemplate создаём метод qTemplate,
+    // куда можно передать объект, содержащий необходимые переменные,
+    // и он вернёт уже обработанный текст.
     $.templates("qTemplate", "#questionTemplate");
     $.templates("aTemplate", "#answerTemplate");
+
     $('#add-question').on("click", function () {
-        templateVars.questionIndex=lastQuestionIndex;
-        templateVars.answerIndex=0;
+        templateVars.questionIndex = lastQuestionIndex;
+        templateVars.answerIndex = 0;
         var currentAnswerBlock = $.render.qTemplate(templateVars);
         lastQuestionIndex++;
-        var $question = $(currentAnswerBlock);
+        var $question = $(currentAnswerBlock); // создание из строкового объекта объекта jQuery
 
-        for(i=0; i<2;i++){
+        for (var i = 0; i < 2; i++) {
             var answer = $.render.aTemplate(templateVars);
             templateVars.answerIndex++;
             $question.children('.add-answer-button').before(answer);
@@ -20,14 +25,13 @@ $(document).ready(function () {
         $('#question-group').append($question);
 
     });
-    $('#question-group').on("click", '.add-answer-button',function () {
 
+    // листнер на весь блок вопросов с выборкой по кнопкам.
+    // Дает возможность не вешать отдельные листнеры на кнопки при добавлении блока
+    $('#question-group').on("click", '.add-answer-button', function () {
         var questionBlock = $(this).parent();
-        var questionId = parseInt(questionBlock.attr('questionnumber'));
-        var answerIndex = parseInt(questionBlock.children('.answer-container').last().attr('answerIndex'));
-
-        templateVars.questionIndex=questionId;
-        templateVars.answerIndex=answerIndex+1;
+        templateVars.questionIndex = parseInt(questionBlock.attr('questionnumber'));
+        templateVars.answerIndex = parseInt(questionBlock.children('.answer-container').last().attr('answerIndex')) + 1;
         var answer = $.render.aTemplate(templateVars);
         $(this).before(answer);
 
